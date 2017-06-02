ee55f510fd1d59b36d7455654ad0ca86f58e3b5f
Mahan Hashemizadeh <mahanhz@gmail.com>
2017-06-02 19:30:05 +0200

    Updated exception handling

--- application/periphery/web/src/main/java/com/example/clean/app/web/controller/exception/GlobalExceptionHandlerController.java
+++ application/periphery/web/src/main/java/com/example/clean/app/web/controller/exception/GlobalExceptionHandlerController.java
@@ -1,10 +1,8 @@
 package com.example.clean.app.web.controller.exception;
 
-import com.example.clean.app.web.controller.MediaTypes;
 import com.example.clean.app.web.controller.ControllerPackageMarker;
+import com.example.clean.app.web.controller.MediaTypes;
 import com.fasterxml.uuid.Generators;
-import com.google.common.collect.ImmutableMap;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.VndErrors;
@@ -16,6 +14,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static com.example.clean.app.web.controller.exception.VndErrorFactory.vndErrors;
@@ -26,35 +27,45 @@ public class GlobalExceptionHandlerController {
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandlerController.class);
 
     public static final String ERROR_ID = "errorId";
+    public static final String ERROR = "error";
     public static final String STATUS = "status";
     public static final String PATH = "path";
     public static final String MESSAGE = "message";
+    public static final String TIMESTAMP = "timestamp";
 
     @ExceptionHandler
     public ResponseEntity<VndErrors> handleException(final HttpServletRequest request,
                                                      final HttpServletResponse response,
                                                      final Throwable throwable) {
 
-        final UUID errorId = Generators.timeBasedGenerator().generate();
-        LOGGER.error("ErrorId: {} references the following error: ", errorId, throwable);
-
-        // TODO: MESSAGE should maybe return a standard value in Production as it may expose too much information.
         final HttpStatus httpStatus = HttpStatus.valueOf(response.getStatus());
-        final ImmutableMap<String, String> errorAttributes = ImmutableMap.of(ERROR_ID, errorId.toString(),
-                                                                             STATUS, httpStatus.toString(),
-                                                                             PATH, request.getRequestURI(),
-                                                                             MESSAGE, defaulted(throwable.getMessage()));
 
-        return new ResponseEntity<>(vndErrors(errorAttributes), headers(), httpStatus);
-    }
-
-    private String defaulted(final String value) {
-        return StringUtils.defaultIfBlank(value, "");
+        return new ResponseEntity<>(vndErrors(errorAttributes(request, response, httpStatus, throwable)),
+                                    headers(),
+                                    httpStatus);
     }
 
     private HttpHeaders headers() {
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaTypes.APPLICATION_VND_ERROR_JSON);
         return headers;
+    }
+
+    private Map<String, String> errorAttributes(final HttpServletRequest request,
+                                                final HttpServletResponse response,
+                                                final HttpStatus httpStatus,
+                                                final Throwable throwable) {
+        final UUID errorId = Generators.timeBasedGenerator().generate();
+        LOGGER.error("ErrorId: {} references the following error: ", errorId, throwable);
+
+        final Map<String, String> errorMap = new HashMap<>();
+        errorMap.put(ERROR_ID, errorId.toString());
+        errorMap.put(ERROR, "Oops! Something went wrong!");
+        errorMap.put(STATUS, httpStatus.toString());
+        errorMap.put(PATH, request.getRequestURI());
+        errorMap.put(MESSAGE, "Record the errorId when reporting this issue");
+        errorMap.put(TIMESTAMP, new Date().toString());
+
+        return errorMap;
     }
 }
