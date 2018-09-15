adc2ca3785f2a5351cf35982ffa4d0d18c819cd9
Mahan Hashemizadeh <mahanhz@gmail.com>
2018-09-15 17:52:04 +0200

    Add checkstyle

--- application/periphery/web/src/main/java/com/example/clean/app/web/controller/MediaTypes.java
+++ application/periphery/web/src/main/java/com/example/clean/app/web/controller/MediaTypes.java
@@ -4,9 +4,11 @@ import org.springframework.http.MediaType;
 
 public final class MediaTypes {
 
+    public static final String APPLICATION_JSON_HAL_VALUE = "application/hal+json";
     public static final String APPLICATION_JSON_V1_VALUE = "application/vnd.example.clean.v1+json";
     public final static String APPLICATION_VND_ERROR_JSON_VALUE = "application/vnd.error+json";
 
+    public static final MediaType APPLICATION_JSON_HAL = MediaType.valueOf(APPLICATION_JSON_HAL_VALUE);
     public static final MediaType APPLICATION_JSON_V1 = MediaType.valueOf(APPLICATION_JSON_V1_VALUE);
     public final static MediaType APPLICATION_VND_ERROR_JSON = MediaType.valueOf(APPLICATION_VND_ERROR_JSON_VALUE);
 }
